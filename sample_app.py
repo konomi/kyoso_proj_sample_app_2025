98f1aa80e546c9ef750d9a843ebfd0bf41f4a8eb
devkonomi_mac <konomi@hdi.ait.kyushu-u.ac.jp>
2025-07-09 13:11:20 +0900

    sheets pub

--- sample_app.py
+++ sample_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import streamlit_calendar as st_calendar
+from streamlit_gsheets import GSheetsConnection
 
 st.set_page_config(
     page_title="Hello",
@@ -14,4 +16,27 @@ df = pd.read_csv('data.csv')
 df
 s =df.describe()
 s
+st.bar_chart(df)
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
 
+    chart_data
+
+st_calendar.calendar() 
+
+
+# streamlit_app.py
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
